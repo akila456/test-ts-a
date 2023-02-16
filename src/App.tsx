fd9c6ce09e35de78719b1658232180dfd31d6b81
akila456 <akilasinne555@gmail.com>
2023-02-16 20:24:38 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <h1 className="text-blue-900">React TS Tailwind</h1>;
+}
+
+export default App;
